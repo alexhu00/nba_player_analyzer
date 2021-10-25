67580bf6cf73ad7549272cc02bdbf782493d21d3
Alex <ah4605@stern.nyu.edu>
2021-10-25 00:15:57 -0400

    Data inputted to table

---
+++ frontend/src/components/AllPlayers.js
@@ -0,0 +1,77 @@
+import React, { useMemo } from "react";
+import NavBar from "../NavBar";
+import { players } from "../nba_player_data_20-21-";
+import { useTable } from "react-table";
+// import nba_player_data_20-21-- from "./nba_player_data_20-21--.json"
+import { COLUMNS } from "./columnsAP";
+
+const AllPlayers = () => {
+  const showData = () => {
+    // const d = JSON.parse(players);
+    console.log("hello");
+    // console.log(Object.entries(players));
+    console.log(players);
+  };
+
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => players, []);
+
+  const tableInstance = useTable({
+    columns,
+    data,
+  });
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = tableInstance;
+
+  return (
+    <div>
+      <button onClick={showData}>Click</button>
+      <table {...getTableBodyProps}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <h1></h1>
+    </div>
+  );
+};
+
+// the home page with the items and the stuff at the bottom
+const Home = () => (
+  <div>
+    <NavBar />
+    <header>
+      <h1>Welcome to All Players</h1>
+
+      <AllPlayers />
+    </header>
+  </div>
+);
+
+export { AllPlayers, Home };
