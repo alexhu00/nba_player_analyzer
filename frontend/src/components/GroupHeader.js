7c945c46597151425f6383b2564c26b12052e601
Javier Saldana <saldanaj27@gmail.com>
2021-11-09 13:45:03 -0500

    added triangles onto the sort by stat button

--- frontend/src/components/GroupHeader.js
+++ frontend/src/components/GroupHeader.js
@@ -4,12 +4,18 @@ import "./GroupHeader.css"
 
 const GroupHeader = () => {
     return (
-        <div className={"group-header"}>
-            <div className={"group-name"}>
+        <div className="group-header">
+            <div className="group-name">
                 <h3>Group Test</h3>
             </div>
-            <div className={"buttons"}>
-                <button className={"sort-button group"}>Sort by Stat</button>
+            <div className="buttons">
+                {/* <div className="arrow-up"></div> */}
+                {/* <div className="arrow-down"></div> */}
+                <button className="sort-button group">
+                    <span className="text">Sort By Stat</span>
+                    <span className="arrow-up"></span>
+                    <span className="arrow-down"></span>
+                </button>
             </div>
         </div>
     )
