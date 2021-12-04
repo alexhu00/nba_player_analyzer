0b9a4f24a8c29ab0da4a1ac938ceca4de359619b
cindykimxp <cindykim100@gmail.com>
2021-12-04 07:24:32 -0500

    inputs for form

--- frontend/src/components/SignupOrLogin.jsx
+++ frontend/src/components/SignupOrLogin.jsx
@@ -1,38 +1,34 @@
-import { useState, React, button } from "react";
+import { useState, React, useEffect } from "react";
 import hoops from "../assets/hoops.png";
 import "./SignupOrLogin.css";
 
 function SignupOrLogin() {
   const [entryType, setEntryType] = useState("Signup");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   //Button response that changes to Login page
   function ChangeToLogin() {
     setEntryType("Login");
+    //Clear inputs
+    setEmail("");
+    setPassword("");
   }
 
   //Button response that changes to Signup page
   function ChangeToSignup() {
     setEntryType("Signup");
+    //Clear inputs
+    setEmail("");
+    setPassword("");
   }
 
-  //Function that changes HTML output for the fields depending on Login or Signup state
-  function Fields(props) {
-    console.log("entryType", props.entryType);
-    if (props.entryType == "Signup") {
-      return (
-        <div className="SignupOrLogin__fields">
-          <div className="SignupOrLogin__email">Email</div>
-          <div className="SignupOrLogin__setpass">Set Password</div>
-          <div className="SignupOrLogin__confirmpass">Confirm Password</div>
-        </div>
-      );
+  function checkPassword(val) {
+    console.log("event", val);
+    if (val == password) {
+      console.log("Yes!");
     } else {
-      return (
-        <div className="SignupOrLogin__fields">
-          <div className="SignupOrLogin__email">Email</div>
-          <div className="SignupOrLogin__setpass">Password</div>
-        </div>
-      );
+      console.log("No!");
     }
   }
 
@@ -44,7 +40,62 @@ function SignupOrLogin() {
         <div className="SignupOrLogin__subtitle">
           Enter your credentials to get access to your account.
         </div>
-        <Fields entryType={entryType}></Fields>
+        <form>
+            {entryType === "Signup" ? (
+            <div className="SignupOrLogin__fields">
+                <input
+                type="text"
+                className="SignupOrLogin__email"
+                placeholder="Email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                required
+                />
+                <input
+                type="text"
+                className="SignupOrLogin__setpass"
+                placeholder="Set password"
+                value={password}
+                onChange={(event) => {
+                    setPassword(event.target.value);
+                }}
+                required
+                />
+                <input
+                type="text"
+                className="SignupOrLogin__setpass"
+                placeholder="Confirm password"
+                onChange={(event) => {
+                    checkPassword(event.target.value);
+                }}
+                required
+                />
+            </div>
+            ) : (
+            <div className="SignupOrLogin__fields">
+                <input
+                type="text"
+                className="SignupOrLogin__email"
+                placeholder="Email"
+                value={email}
+                onChange={(event) => {
+                    setEmail(event.target.value);
+                }}
+                required
+                />
+                <input
+                type="text"
+                className="SignupOrLogin__setpass"
+                placeholder="Password"
+                value={password}
+                onChange={(event) => {
+                    setPassword(event.target.value);
+                }}
+                required
+                />
+            </div>
+            )}
+        </form>
         <button
           className="SignupOrLogin__signupbtn"
           onClick={() => ChangeToSignup()}
