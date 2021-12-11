a34f4b4fc4e5325ac7a5fbafcc0eee196b206d8a
cindykimxp <cindykim100@gmail.com>
2021-12-10 20:30:49 -0500

    hashing password

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,11 +2,15 @@
 const express = require('express')
 const router = express.Router()
 const signUpTemplateCopy = require('../models/user');
+const bcrypt = require('bcrypt')
 
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
+  const saltPassword = await bcrypt.genSalt(10)
+  const securePassword = await bcrypt.hash(req.body.password, saltPassword)
+
   const signedUpUser = new signUpTemplateCopy({
     email: req.body.email,
-    password: req.body.password,
+    password: securePassword,
     groups: req.body.groups
   })
   signedUpUser.save()
